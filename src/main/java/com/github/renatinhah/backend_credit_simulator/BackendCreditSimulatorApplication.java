10820141f5afa7a453e754c1aa00824c28bdf269
renata <renata.ufop@yahoo.com.br>
2025-09-18 16:01:00 -0300

    Add Spring Boot project structure and update .gitignore and .gitattributes

---
+++ src/main/java/com/github/renatinhah/backend_credit_simulator/BackendCreditSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.renatinhah.backend_credit_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCreditSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCreditSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/renatinhah/backend_credit_simulator/BackendCreditSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renatinhah.backend_credit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCreditSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
