882d6b0531d3049c505b7398b65c32749792dce7
renata <renata.ufop@yahoo.com.br>
2025-09-20 12:56:40 -0300

    feat: add service with the main logic of credit simulation calculations

---
+++ src/main/java/com/github/renatinhah/backend_credit_simulator/service/LoanSimulationService.java
@@ -0,0 +1,72 @@
+package com.github.renatinhah.backend_credit_simulator.service;
+
+import com.github.renatinhah.backend_credit_simulator.dto.LoanSimulationDetails;
+import com.github.renatinhah.backend_credit_simulator.dto.LoanSimulationRequest;
+import com.github.renatinhah.backend_credit_simulator.dto.LoanSimulationResponse;
+import com.github.renatinhah.backend_credit_simulator.model.enums.InterestRateEnum;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.Period;
+
+@Service
+public class LoanSimulationService {
+    public static final int MONTHS_IN_YEAR = 12;
+
+    public LoanSimulationResponse simulate(LoanSimulationRequest request) {
+        try {
+            int age = calculateAge(request.getBirthDate());
+            InterestRateEnum rate = InterestRateEnum.getRateByAge(age);
+
+            BigDecimal monthlyPayment = calculateInstallment(request, new BigDecimal(rate.rate));
+
+            LoanSimulationDetails details = calculateSimulationDetails(request, monthlyPayment);
+
+            return createLoanSimulationResponse(monthlyPayment, details);
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
+        }
+        return null;
+    }
+
+    private int calculateAge(LocalDate birthDate) {
+        return Period.between(birthDate, LocalDate.now()).getYears();
+    }
+
+    private BigDecimal calculateInstallment(LoanSimulationRequest request, BigDecimal rate) {
+        BigDecimal monthRate = rate.divide(new BigDecimal(MONTHS_IN_YEAR), RoundingMode.HALF_UP);
+
+        BigDecimal loanAmount = request.getLoanAmount();
+        int numberOfPayments = request.getPaymentTermInMonths();
+
+        BigDecimal sumRateOne = new BigDecimal(1).add(monthRate); // (1 + r)
+        BigDecimal potencia = sumRateOne.pow(-numberOfPayments, MathContext.DECIMAL128); // (1 + r)^(-n)
+        BigDecimal divisor = new BigDecimal(1).subtract(potencia); // 1 - (1 + r)^(-n)
+        BigDecimal dividend = loanAmount.multiply(monthRate); // (PV * r)
+
+        return dividend.divide(divisor, MathContext.DECIMAL128); // (PV * r) / (1 - (1 + r)^(-n))
+    }
+
+    private LoanSimulationDetails calculateSimulationDetails(LoanSimulationRequest request, BigDecimal monthlyPayment) {
+        BigDecimal totalAmountToPay = monthlyPayment.multiply(new BigDecimal(request.getPaymentTermInMonths()));
+        totalAmountToPay = totalAmountToPay.setScale(2, RoundingMode.HALF_UP);
+
+        BigDecimal totalInterest = totalAmountToPay.subtract(request.getLoanAmount());
+        totalInterest = totalInterest.setScale(2, RoundingMode.HALF_UP);
+
+        return new LoanSimulationDetails(totalAmountToPay, totalInterest);
+    }
+
+    private LoanSimulationResponse createLoanSimulationResponse(BigDecimal monthlyPayment,LoanSimulationDetails details) {
+        LoanSimulationResponse response = new LoanSimulationResponse();
+        response.setTotalAmountToPay(details.getTotalAmountToPay());
+        response.setMonthlyInstallment(monthlyPayment.setScale(2, RoundingMode.HALF_UP));
+        response.setTotalInterest(details.getTotalInterest());
+
+        return response;
+    }
+
+}
